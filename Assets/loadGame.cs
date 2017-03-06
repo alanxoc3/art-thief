9b2837a5979165cd3061b2fe85f68cfc4996eb60
Alan Morgan <alanxoc3@gmail.com>
2017-03-05 18:48:39 -0700

    Title Screen is Added.

---
+++ Assets/loadGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class loadGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool isSpace = Input.GetKeyDown (KeyCode.Space);
+
+		if (isSpace) {
+			SceneManager.LoadScene ("Museum");
+		}
+	}
+}
